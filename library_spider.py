0622794f1937e044e0a19c3418166386da236e42
nickwu96 <46734518+nickwu96@users.noreply.github.com>
2020-03-18 11:57:43 +0800

    Update to Ver2.2
    更新至Ver2.2，增加同时检索纸质资源和多条电子资源的功能，同时修复了一些问题

--- library_spider.py
+++ library_spider.py
@@ -13,7 +13,12 @@ def get(url, ebook=False):  # 通过selenium（webdriver）获取网页全部内
     driver.get(url)
     time.sleep(5)  # 等待5s使得全部信息加载完成
     if ebook:
-        driver.switch_to.frame("iFrameResizer0")  # 若为电子资源需要切换到iframe后再导出源代码
+        try:
+            driver.switch_to.frame("iFrameResizer0")  # 若为电子资源需要切换到iframe后再导出源代码
+        except:
+            driver.get(url)
+            time.sleep(5)
+            driver.switch_to.frame("iFrameResizer0")  # 若为电子资源需要切换到iframe后再导出源代码
     soup = BeautifulSoup(driver.page_source, 'html.parser')  # 使用beautifulsoup解析全部网页内容
     driver.close()  # 关闭driver
     return soup
@@ -36,21 +41,40 @@ def search(isbn):
     else:
         # 已经检索到馆藏信息
         # 首先判断这条信息是不是“图书”，若为“评论”等则为无效信息
+        book_ans = ''
         for i in soup.find_all('div', class_='list-item-wrapper'):
             j = i.find_all('span')
             if j[4].text == '图书' or j[2].text == '图书':  # 首先判断这一条信息是否为“图书”
-                if j[-4].text[-4:] == '不可获取':  # 若提示不可获取意味着这本书还在订购，返回订购中的状态
-                    return '订购中\n'
-                if j[-1].text[-4:] == '在线访问':
+                is_multi = soup.find('span', attrs={'translate': 'nui.frbrversion.found'})  # 增加一个标签判断是否有多个副本信息
+                if is_multi:
+                    return '存在多个版本，建议根据其他信息手动核查！\n'
+
+                elif j[-4].text[-4:] == '不可获取':  # 若提示不可获取有两种情况：若有索书号则该图书正在借阅中，应返回索书号；否则为这本书正在订购中
+                    number = soup.find('span', class_='best-location-delivery locations-link').text  # 获取索书号
+                    if not number:  # 若number=None，也就是找不到索书号，则是订购中的状态
+                        book_ans += '订购中;'
+                    else:
+                        book_ans += number + ';'
+
+                elif j[-1].text[-4:] == '在线访问':
                     ebook_url = i.find('a')['href']  # 若为电子资源，需要跳转到新的界面查询其数据库
                     soup = get(ebook_url, ebook=True)
-                    return '电子资源：{}\n'.format(soup.find('a', attrs={'target': '_blank'}).text)
-                if j[-1].text[-4:] == '在线全文':
-                    return '在线全文，目前未支持！\n'
+                    book_ans += '电子图书-'
+                    for i in soup.find_all('a', attrs={'target': '_blank'}):
+                        if i.text != '':
+                            book_ans += i.text[:i.text.find(' ')] + ';'  # 近提取电子资源数据库的第一个关键词
+
+                elif j[-1].text[-4:] == '在线全文':
+                    book_ans += '在线全文;'
                 else:  # 如果不是订购中，也不是电子资源，则直接返回索书号
-                    number = soup.find('span', class_='best-location-delivery locations-link').text  # 获取索书号
-                    return '{}\n'.format(number)
-        return 'N\n'  # 若检索到的信息都不是图书则返回N
+                    number = soup.find('span', class_='best-location-delivery locations-link')
+                    if number:
+                        book_ans += number.text + ';'  # 获取索书号
+
+        if book_ans:
+            return '{}\n'.format(book_ans[:-1])
+        else:
+            return 'N\n'  # 若检索到的信息都不是图书则返回N
 
 
 if __name__ == '__main__':
@@ -60,12 +84,10 @@ if __name__ == '__main__':
     ans = []  # 最终生成的结果
     num = 1  # 计数器
     for i in contents:
-        for iter_num in range(5):  # 迭代5次，避免因网络不好未加载出搜索结果
+        for iter_num in range(3):  # 如果遇到没有馆藏，三次查询，防止因为网页未加载完成导致误判断为无馆藏
             result = search(i)  # 调用查询函数，result记录了当前图书查询返回的结果
-            if result is not None:  # 找到搜索结果后跳出循环
+            if result != 'N\n':
                 break
-        if result is None:
-            result = '抱歉，暂未查询到信息！\n'
         print('[{}/{}]ISBN:{},{}'.format(num, len(contents), i.rstrip(), result), end='')  # 输出当前状态，第num条图书，其状态为..
         ans.append(result)
         num += 1
